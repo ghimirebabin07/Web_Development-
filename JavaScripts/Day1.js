87c6bd0a62dbceb8cf20e0ede6018140a5777472
ghimirebabin07 <240107babin@cosmoscollege.edu.np>
2026-02-16 21:04:37 +0545

    Code of the Day1

---
+++ JavaScripts/Day1.js
@@ -0,0 +1,16 @@
+//print in console
+console.log("Hello Bain,Welcome to javascript");
+//variables 
+let name = "Babin";
+let age = 19;
+const country = "Nepal";
+console.log("Name:",name);
+console.log('Age:',age);
+console.log('Country:',country);
+//data types
+let isStudent = true;
+let value = null;
+let x;
+console.log("Is Student:",isStudent);
+console.log("Value:",value);
+console.log("X:",x);
